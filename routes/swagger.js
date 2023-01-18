de5d75820661b44d1e58345eddc0af422605d95f
rodell1983 <ode21001@byui.edu>
2023-01-18 12:58:26 -0800

    revert

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 
-//router.use('/api-docs', swaggerUi.serve);
-//router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
